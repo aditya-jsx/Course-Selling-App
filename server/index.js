6a19ef8e3d2ce878cf37e1ea10ec97a2cdeeec95
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-08-05 01:10:21 +0530

    setting up the project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+
+
+
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000);
